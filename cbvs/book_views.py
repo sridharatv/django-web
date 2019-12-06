4dfe156c0881eb29ad4fad1ed3d3eeda18b9ff83
Sridhara T V <sridharatv@gmail.com>
2019-12-06 19:35:58 +0530

    few css changes

--- cbvs/book_views.py
+++ cbvs/book_views.py
@@ -5,33 +5,46 @@ from django.views.generic import (View,
                                   UpdateView,
                                   DeleteView)
 from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import  LoginRequiredMixin
 from . import models
 
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     # by default ListView returns book_list context dict.
     # changing that to 'libraries'
     context_object_name = 'books'
     model = models.Book
+    login_url = '/login/login/'
+    redirect_field_name = 'cbvs/book_list.html'
 
-class BookDetailView(DeleteView):
+class BookDetailView(LoginRequiredMixin, DeleteView):
     # by default DetailView returns 'book' context dict
     # changing that to 'book_detail'
     context_object_name = 'book_detail'
     model = models.Book
     template_name = 'cbvs/book_detail.html'
+    login_url = '/login/login/'
+    redirect_field_name = 'cbvs/book_detail.html'
 
-class BookCreateView(CreateView):
+class BookCreateView(LoginRequiredMixin, CreateView):
     model = models.Book
     fields = '__all__'
+    login_url = '/login/login/'
+    redirect_field_name = 'cbvs/book_list.html'
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = models.Book
     fields = '__all__'
+    login_url = '/login/login'
+    redirect_field_name = 'cbvs/book_list.html'
 
-class BookDeleteView(DeleteView):
+class BookDeleteView(LoginRequiredMixin, DeleteView):
     model = models.Book
     success_url = reverse_lazy("cbvs:book_list")
+    login_url = '/login/login/'
+    redirect_field_name = 'cbvs/book_list.html'
+
+
 
 
--- login/forms.py
+++ login/forms.py
@@ -8,10 +8,19 @@ class UserForm(forms.ModelForm):
     class Meta():
         model = User
         fields = ('username', 'email', 'password', 'first_name', 'last_name')
-
+        widgets = {
+            'password': forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password', 'placeholder': 'Your Password'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'name': 'username', 'placeholder': 'User Name'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'name': 'first_name', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'name': 'last_name', 'placeholder': 'Last Name'}),
+            'email': forms.TextInput(attrs={'class': 'form-control', 'name': 'email', 'placeholder': 'Email'}),
+        }
 
 class CustomUserForm (forms.ModelForm):
     class Meta():
         model = CustomUser
         fields = ('url', 'picture')
-
+        widgets = {
+            'url' : forms.URLInput(attrs={'class':'form-control', 'name':'url', 'placeholder':'Enter Your URL'}),
+            'picture': forms.FileInput(),
+        }
